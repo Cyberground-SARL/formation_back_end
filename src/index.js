37dc17e634d64670631159e223be1900aac59ab4
EL-MEHDI LOUKACH <you@example.com>
2022-09-18 17:39:09 +0100

    app initialization

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const server = require('express')()
+const PORT = process.env.PORT || 3333
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet')
+
+dotenv.config({path: './config.env'})
+//config general
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors({
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    origin: ["http://localhost:3000" /*"https://NAMEPROJECT.netlify.server"*/],
+}));
+server.use(helmet());
+
+// connection to database
+const connectDB = require('./config/db')
+connectDB();
+
+server.listen(PORT, ()=>{
+    console.log(`API IS RUNNING ON PORT ${PORT}`)
+})

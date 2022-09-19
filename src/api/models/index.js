d8c36ce3a1a2bb3bbb35d31ab28a27cc0cf7aeed
Oussam-lah <112032026+Oussa-ma-01@users.noreply.github.com>
2022-09-19 20:26:09 +0100

    Update index.js

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -1,1 +1,16 @@
 
+// pour define any resourse
+const mongose = require('mongose')
+const Schematask = mongoose.Schema({
+    text: {
+        type : String ,
+        required :[true,'please add a text value'],
+    },
+},
+{
+    timestamps:true,
+}
+)
+module.expports = mongose.model('task',Schematask)
+
+

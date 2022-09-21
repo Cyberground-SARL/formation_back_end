e979aa210f975d13382b0f8cd9b15cccc93e4cbc
Oussam-lah <112032026+Oussa-ma-01@users.noreply.github.com>
2022-09-21 22:30:34 +0100

    Update index.js

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,13 +1,47 @@
-const express =require('express')
+const express = require('express')
+// cette instruction est équivalent à
+// import React from 'react'
 const router = express.Router()
+const {GETALL1,GETALL2,GETALL3,GETALL4,GETALL5}= require ('../controllers/index')
+//cette fonction est express.Router est pour créer un nouvelle objet router
 //const {getGoals} = require ('../controllers/index.js')
+// On peut faires d'autre choces comme suite :
+/*
+var  express = require('express);
+var app = express();
+var PORT = 3000;
 
+// Multiple routing 
+var router1= express.Router()
+var router2 = express.Router();
+var router3=  express.Router()
+
+router1.get('/user',function (req,res)){
+console.log("User router working");
+res.end();
+});
+
+router2.get('/admin', function (req, res, next) {
+    console.log("Admin Router Working");
+    res.end();
+});
+
+router2.get('/student', function (req, res, next) {
+    console.log("Student Router Working");
+    res.end();
+});
+
+app.use(router1);
+app.use(router2);
+app.use(router3);
+
+*/
  router.get('/',(req, res) =>{
     GETALL1(req.body)
  })
 
  router.get('/:id',(req, res) =>{
-    GETTALL2(req.body)
+    GETALL2(req.body)
  })
  
  router.post('/',(req, res) =>{
@@ -18,12 +52,11 @@ const router = express.Router()
     GETALL4(req.body)
  })
 
- // la requete delete non travaille pas
- 
  router.delete('/:id',(req, res) =>{
-    GETALL(req.body)
+    GETALL5(req.body)
  })
 
+ module.exports = router;
  
  
  
